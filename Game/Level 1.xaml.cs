071ca743db1b5c694e77b2bb6601fb8c1cea4e1d
Sasha <sdrozdiuk12@gmail.com>
2018-12-12 02:39:35 +0300

    +smth f-king anreal....

--- Game/Level 1.xaml.cs
+++ Game/Level 1.xaml.cs
@@ -18,12 +18,32 @@ namespace Game
     /// Interaction logic for Start.xaml
     /// </summary>
     /// 
- 
+
     public partial class Start : Window
     {
         public Start()
         {
             InitializeComponent();
+
+        }
+
+        private void Ellipse_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var temp = (new Point(Mouse.GetPosition(sender as Ellipse).X, Mouse.GetPosition(sender as Ellipse).Y));
+            Polyline pol = new Polyline();
+            pol.Points.Add(temp);
+            pol.Points.Add(new Point(Mouse.GetPosition(sender as Ellipse).X + 100, Mouse.GetPosition(sender as Ellipse).Y + 100));
+            pol.Points.Add(new Point(Mouse.GetPosition(sender as Ellipse).X + 50, Mouse.GetPosition(sender as Ellipse).Y + 250));
+            pol.Fill = (sender as Ellipse).Fill;
+            pol.Stroke = (sender as Ellipse).Stroke;
+            pol.StrokeThickness = 7;
+            Can.Children.Add(pol);
+            Ellipse_MouseUp(sender, e);
+        }
+
+        private void Ellipse_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Test");
         }
     }
 }
