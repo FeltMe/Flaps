4b7c3736fe266d161c7206996360b66274353479
sddrozd <sddrozd@A36COMP4>
2018-12-12 18:29:12 +0200

    + micro funct

--- Game/Level 1.xaml.cs
+++ Game/Level 1.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
@@ -27,26 +28,28 @@ namespace Game
 
         }
 
-        private void Ellipse_MouseDown(object sender, MouseButtonEventArgs e)
+
+
+        private void Ellipse_MouseMove(object sender, MouseEventArgs e)
         {
-            var kek = (sender as Canvas);
-            
-            var temp = new Point(35, 30);
-            Polyline pol = new Polyline();
-            pol.Fill = Brushes.Green;
-            pol.Points.Add(temp);
-            pol.Points.Add(new Point(temp.X + 45, temp.Y));
-            //pol.Points.Add(new Point(Mouse.GetPosition(sender as Ellipse).X + 32, Mouse.GetPosition(sender as Ellipse).Y + 32));
+            var elipse = (sender as Ellipse);
+            var l = Canvas.GetLeft(elipse);
+            var t = Canvas.GetTop(elipse);
+
+            var point = new Point(e.GetPosition(elipse).X, e.GetPosition(elipse).Y);
+            Polyline pol = new Polyline
+            {
+                Fill = Brushes.Green
+            };
+            Canvas.SetLeft(pol, l);
+            Canvas.SetTop(pol, t);
+            pol.Points.Add(point);
+            pol.Points.Add(new Point(e.GetPosition(elipse).X, e.GetPosition(elipse).Y));
+            pol.Points.Add(new Point(Mouse.GetPosition(sender as Ellipse).X + 32, Mouse.GetPosition(sender as Ellipse).Y + 32));
             pol.Fill = (sender as Ellipse).Fill;
-            pol.Stroke = (sender as Ellipse).Stroke;
+            pol.Stroke = (sender as Ellipse).Fill;
             pol.StrokeThickness = 7;
             Can.Children.Add(pol);
-            //Ellipse_MouseUp(sender, e);
-        }
-
-        private void Ellipse_MouseUp(object sender, MouseButtonEventArgs e)
-        {
-            //MessageBox.Show("Test");
         }
     }
 }
