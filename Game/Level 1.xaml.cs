47cd0e02013adba5d6e79c8173726d1fddc8ede1
sddrozd <sddrozd@A36COMP4>
2018-12-12 19:42:54 +0200

    Keki4

--- Game/Level 1.xaml.cs
+++ Game/Level 1.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Media;
+
 
 namespace Game
 {
@@ -22,21 +24,63 @@ namespace Game
 
     public partial class Start : Window
     {
+        private MediaPlayer mp = new MediaPlayer();
+        Point Defoult_Green_point_1 = new Point();
+        Point Defoult_Green_point_2 = new Point();
+        Point Defoult_Red_point_1 = new Point();
+        Point Defoult_Red_point_2 = new Point();
+        Point Defoult_Yellow_point_1 = new Point();
+        Point Defoult_Yellow_point_2 = new Point();
+        Point Defoult_Orange_point_1 = new Point();
+        Point Defoult_Orange_point_2 = new Point();
+
         public Start()
         {
             InitializeComponent();
-
+            //mp.Open(, UriKind.RelativeOrAbsolute);
+            mp.Play();
         }
 
-
+       //private void Ellipse_MouseDown(object sender, MouseEventArgs e)
+       //{
+       //
+       //   // var elipse = (sender as Ellipse);
+       //   // var l = Canvas.GetLeft(elipse);
+       //   // var t = Canvas.GetTop(elipse);
+       //   // var point1 = new Point(45, 45);
+       //   // var point2 = new Point(90, 45);
+       //   // Line line = new Line
+       //   // {
+       //   //     X1 = 45,
+       //   //     X2 = 90,
+       //   //     Y1 = 45,
+       //   //     Y2 = 45
+       //   // };
+       //   // Canvas.SetLeft(line, l);
+       //   // Canvas.SetTop(line, t);
+       //   // line.Fill = Brushes.Black;
+       //   // line.StrokeThickness = 6;
+       //   // Can.Children.Add(line);
+       //    
+       //
+       //
+       //  
+       //}
 
         private void Ellipse_MouseMove(object sender, MouseEventArgs e)
         {
             var elipse = (sender as Ellipse);
+            Polyline polyline = new Polyline();
+            Point first_point = new Point(e.GetPosition(elipse).X, e.GetPosition(elipse).Y);
+            
+        }
+
+        private void Ellipse_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var elipse = (sender as Ellipse);
             var l = Canvas.GetLeft(elipse);
             var t = Canvas.GetTop(elipse);
-
-            var point = new Point(e.GetPosition(elipse).X, e.GetPosition(elipse).Y);
+            var point = new Point(45, 45);
             Polyline pol = new Polyline
             {
                 Fill = Brushes.Green
@@ -45,10 +89,10 @@ namespace Game
             Canvas.SetTop(pol, t);
             pol.Points.Add(point);
             pol.Points.Add(new Point(e.GetPosition(elipse).X, e.GetPosition(elipse).Y));
-            pol.Points.Add(new Point(Mouse.GetPosition(sender as Ellipse).X + 32, Mouse.GetPosition(sender as Ellipse).Y + 32));
+            pol.Points.Add(new Point(Mouse.GetPosition(sender as Ellipse).X, Mouse.GetPosition(sender as Ellipse).Y));
             pol.Fill = (sender as Ellipse).Fill;
-            pol.Stroke = (sender as Ellipse).Fill;
-            pol.StrokeThickness = 7;
+            pol.Stroke = Brushes.Black;
+            pol.StrokeThickness = 10;
             Can.Children.Add(pol);
         }
     }
