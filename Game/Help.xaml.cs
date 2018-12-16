a55a7c914c5c37cddf4ff35b1acdd85ac723207f
alex kunderenko <kunderenko2@gmail.com>
2018-12-16 22:25:16 +0200

    Add Helper

--- Game/Help.xaml.cs
+++ Game/Help.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Game
 {
@@ -19,10 +20,41 @@ namespace Game
     /// </summary>
     public partial class Help : Window
     {
+        public MediaPlayer _Help;
         public Help()
         {
             InitializeComponent();
-            
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += timer_Tick;
+            timer.Start();
+
+
+        }
+        void timer_Tick(object sender, EventArgs e)
+        {
+            if (mePlayer.Source != null)
+            {
+                if (mePlayer.NaturalDuration.HasTimeSpan)
+                    lblStatus.Content = String.Format("{0} / {1}", mePlayer.Position.ToString(@"mm\:ss"), mePlayer.NaturalDuration.TimeSpan.ToString(@"mm\:ss"));
+            }
+            else
+                lblStatus.Content = "No file selected...";
+        }
+        private void btnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            mePlayer.Play();
+        }
+
+        private void btnPause_Click(object sender, RoutedEventArgs e)
+        {
+            mePlayer.Pause();
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            mePlayer.Stop();
         }
     }
 }
